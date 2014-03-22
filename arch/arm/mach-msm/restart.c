ed5a584852e1a3f9157f9369655fec88574b9375
forfivo <forfivo@gmail.com>
2014-03-22 20:04:05 -0300

    Removed debugs

--- arch/arm/mach-msm/restart.c
+++ arch/arm/mach-msm/restart.c
@@ -63,8 +63,10 @@ static int restart_mode;
 #ifndef CONFIG_SEC_DEBUG
 void *restart_reason;
 #endif
+#ifdef CONFIG_SEC_DEBUG
 int kernel_sec_get_debug_level(void);
 #define KERNEL_SEC_DEBUG_LEVEL_LOW      (0x574F4C44)
+#endif
 int pmic_reset_irq;
 static void __iomem *msm_tmr0_base;
 
@@ -383,7 +385,7 @@ static int __init msm_pmic_restart_init(void)
 
 #if defined(CONFIG_MACH_JF_VZW) || defined(CONFIG_MACH_MELIUS)
 	return 0;
-#else
+#elif defined(CONFIG_SEC_DEBUG)
 	if (kernel_sec_get_debug_level() != KERNEL_SEC_DEBUG_LEVEL_LOW)
 		return 0;
 #endif
